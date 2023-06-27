ee42309f5f402509bc0a17b6dc4b997597ff9eb0
Argavyon <75849479+Argavyon@users.noreply.github.com>
2023-06-27 19:21:21 +0200

    Update check_forums.js

--- getUpdates/check_forums.js
+++ getUpdates/check_forums.js
@@ -24,9 +24,9 @@ async function fetchNewPostAndUpdate() {
 
     if (threadHRef === lastURL) {
         core.warning('No new posts found');
-        core.setOutput('new_post', 0);
+        core.setOutput('new_post', false);
     } else {
-        core.setOutput('new_post', 1);
+        core.setOutput('new_post', true);
         core.setOutput('post_url', threadHRef);
 
         const response2 = await fetch(proxy + 'https://forums.ddo.com' + threadHRef);
